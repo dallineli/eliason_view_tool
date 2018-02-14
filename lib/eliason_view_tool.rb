0b2d165377d5e1fe4b42f4816ca26ea689e047e7
Dallin Eliason <dallineliason@Dallins-MacBook-Pro.local>
2018-02-13 18:59:10 -0700

    Eli Tool initial commit

---
+++ lib/eliason_view_tool.rb
@@ -0,0 +1,5 @@
+require "eliason_view_tool/version"
+require "eliason_view_tool/renderer"
+module EliasonViewTool
+  # Your code goes here...
+end
---
+++ lib/eliason_view_tool/version.rb
@@ -0,0 +1,3 @@
+module EliasonViewTool
+  VERSION = "0.1.0"
+end
